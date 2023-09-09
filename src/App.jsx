c9d74dcd9d43ad0ddf710cc40d883981904e5ff8
Boyeon Ihn <boyeonihn@gmail.com>
2023-09-09 17:42:20 +0200

    feat: create favoriteList context and wrap around App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 import './App.css';
 
+export const FavoriteListContext = createContext([]);
 function App() {
   const [theme, setTheme] = useState('light');
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = ({ name, id, coverImage }) => {
+    favorites.find((item) => item.name === name)
+      ? setFavorites((prevList) =>
+          prevList.filter((item) => item.name !== name)
+        )
+      : setFavorites([...favorites, { name, id, coverImage }]);
+  };
 
   const toggleTheme = () => {
     setTheme((current) => (current === 'light' ? 'dark' : 'light'));
   };
 
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <div className="w-full h-full">
+      <FavoriteListContext.Provider value={{ favorites, toggleFavorite }}>
+        <Header />
+        <Outlet />
+      </FavoriteListContext.Provider>
+    </div>
   );
 }
 
