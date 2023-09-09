7dfad404771f461198ec17f8a6b69e2497897fb2
Boyeon Ihn <boyeonihn@gmail.com>
2023-09-09 15:54:06 +0200

    feat: implement useEffect fetch API call for Home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from 'react';
+import { Character } from '../components/Character';
+
 const Home = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  const getCharacters = async () => {
+    const response = await fetch(
+      'https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023'
+    );
+    const json = await response.json();
+    console.log(json.data.results);
+
+    setData(json.data.results);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
 };
 
 export default Home;
