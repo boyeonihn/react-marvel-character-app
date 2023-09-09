09e9ec8cf463e559b177b1b37a2b10ea30bc7f6e
Boyeon Ihn <boyeonihn@gmail.com>
2023-09-09 15:52:34 +0200

    feat: implement useEffect hook for fetchAPI request

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -6,6 +6,21 @@ const Detail = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [character, setCharacter] = useState();
   const { id } = useParams();
+
+  const getCharacter = async () => {
+    const response = await fetch(
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}`
+    );
+
+    const json = await response.json();
+    setCharacter(json.data.results[0]);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
 };
 
 export default Detail;
