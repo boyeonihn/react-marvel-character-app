837ab3dd2b048aff978b36078fa669309d6a294c
Boyeon Ihn <boyeonihn@gmail.com>
2023-09-09 21:52:45 +0200

    feat: utilize FavoriteData context in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
+import { FavoriteListContext } from '../App';
+import { useContext } from 'react';
 
 const Header = () => {
+  const { favorites } = useContext(FavoriteListContext);
+
   return (
     <header className="w-full m-1 font-sans font-bold bg-black flex flex-col items-center">
-      <h1 className="m-5 w-2/5 text-red-50 bg-red-600 py-3 tracking-[-.105em] text-8xl">
+      <h1 className="text-center m-5 w-96 text-red-50 bg-red-600 py-3 tracking-[-.105em] text-8xl">
         <Link to="/">MARVEL</Link>
       </h1>
       <nav>
-        <Link to={`/favorite-list`}>Favorite Characters {`(${0})`}</Link>
+        <Link to={`/favorite-list`}>
+          Favorite Characters {`(${favorites.length})`}
+        </Link>
       </nav>
     </header>
   );
